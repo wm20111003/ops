fe3f068f1a34a0625f227c41e80979ea98258138
wm20111003 <wm20111003@163.com>
2016-11-07 22:05:43 +0800

    upd app

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') }, //坑：不能用叹号链接，必须写成这种格式
             { test: /\.less$/, loader: ExtractTextPlugin.extract('css!less') },
             { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel' },
-            { test: /\.(png|jpg)$/, loader: 'url?limit=8192&name=img/[name].[ext]' },
+            { test: /\.(png|jpg)$/, loader: 'url?limit=8192&name=../img/[name].[ext]' },
             { test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'url' }
         ]
     },
